69731708e3c3c4849d17f0da612978783553186f
Andejy <104570789+Andejy@users.noreply.github.com>
2022-10-05 10:24:33 -0400

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -9,3 +9,36 @@ window.addEventListener("scroll", () => {
       (cardTwo.style.animation = "card-animation 3s "),
       (cardOne.style.animation = "card-animation 3s ");
 });
+window.addEventListener("scroll", () => {
+  let card = document.querySelector(".card"),
+    cardTwo = document.getElementById("two"),
+    cardOne = document.getElementById("one"),
+    cardPosition = card.getBoundingClientRect().top;
+  let tamanoDepantalla = window.innerHeight * 0.3;
+  if (cardPosition < tamanoDepantalla)
+    (card.style.animation = "card-animation 3s "),
+      (cardTwo.style.animation = "card-animation 3s "),
+      (cardOne.style.animation = "card-animation 3s ");
+});
+
+
+const btnModalClose = document.querySelector(".modal-close"),
+ modal = document.querySelector(".Modal"),
+inputBtn = document.querySelector(".input-btn"), 
+modalBtn = document.querySelector(".modal-btn")
+
+
+
+modalBtn.addEventListener("click", () => {
+  modal.classList.add("modalClosed");
+});
+
+
+//boton que abra el modal 
+inputBtn.addEventListener("click", () => {
+  modal.classList.remove("modalClosed");
+});
+
+btnModalClose.addEventListener("click", () => {
+  modal.classList.toggle("modalClosed")
+})
